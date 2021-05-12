3a29e2e289b35a36b357fb99a6e218d591939933
Anna <nitlara@gmail.com>
2021-05-12 23:06:21 +0200

    Update Contacts.js

--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
 
@@ -10,6 +9,7 @@ export const Contacts = () => {
 	const [state, setState] = useState({
 		showModal: false
 	});
+
 	const allContacts = store.contacts.map((element, index) => {
 		return (
 			<ContactCard
@@ -24,7 +24,7 @@ export const Contacts = () => {
 		);
 	});
 
-	console.log(store.contacts);
+	//console.log(store.contacts);
 	return (
 		<div className="container">
 			<div>
